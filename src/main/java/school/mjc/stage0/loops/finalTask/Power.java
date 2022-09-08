10ec4a8ce0567243c872183b0b17876528c5e8ea
Artsiom Trushko <artemchiik@gmail.com>
2022-09-08 23:51:04 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int numberPower = 1;
+        for (int i = 0; i < power; i++) {
+            numberPower *= numberToPrint;
+        }
+        System.out.println(numberPower);
     }
 
     public static void main(String[] args) {
